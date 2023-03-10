77137d6dc329e5ee2e3c28d09242d819923628a2
Luz Alba Posse <luzalbaposse@gmail.com>
2023-03-10 11:13:28 -0300

    New exercise resolved

--- Guía1/src/ej2_merge.c
+++ Guía1/src/ej2_merge.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+Implementar una función que tome dos arreglos de enteros (int) ordenados de menor a mayor
+y retorne un nuevo arreglo que combine ambos arreglos de forma ordenada. La función se debe
+llamar int* merge(int* A, int sizeA, int* B, int sizeB), donde A y B son punteros a
+arreglos y sizeA y sizeB sus respectivos tamaños.
+Ejemplo: merge([2,3,5,7,9,9], 6, [8,10,12], 3) → [2,3,5,7,8,9,9,10,12]
+
+*/
 int* merge(int* A, int sizeA, int* B, int sizeB) {
     int* result = (int*) malloc((sizeA + sizeB) * sizeof(int));
     int i = 0; // Índice para recorrer el arreglo A
@@ -36,6 +45,43 @@ int* merge(int* A, int sizeA, int* B, int sizeB) {
     return result;
 }
 
+/*
+b. Modificar el ítem anterior para que los números de los arreglos sean de tipo float.
+*/
+float* merge_b(float* A, int sizeA, float* B, int sizeB) {
+    float* result = (float*) malloc((sizeA + sizeB) * sizeof(float));
+    int i = 0; // Índice para recorrer el arreglo A
+    int j = 0; // Índice para recorrer el arreglo B
+    int k = 0; // Índice para recorrer el arreglo resultante
+    
+    // Recorrer ambos arreglos y agregar el elemento más pequeño al resultado
+    while (i < sizeA && j < sizeB) {
+        if (A[i] <= B[j]) {
+            result[k] = A[i];
+            i++;
+        } else {
+            result[k] = B[j];
+            j++;
+        }
+        k++;
+    }
+    
+    // Agregar los elementos restantes de A, si es que hay
+    while (i < sizeA) {
+        result[k] = A[i];
+        i++;
+        k++;
+    }
+    
+    // Agregar los elementos restantes de B, si es que hay
+    while (j < sizeB) {
+        result[k] = B[j];
+        j++;
+        k++;
+    }
+    
+    return result;
+}
 int main() {
     
     // -- Descomentar para probar --
--- Guía1/src/ej3_array_strings.c
+++ Guía1/src/ej3_array_strings.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Considerar un arreglo de punteros a string.
+a. Implementar la función char* longest(char* v[], int size), que toma un arreglo 
+de punteros a string y retorna el puntero al string m´as largo. De existir 
+más de un string que cumpla
+la condición, se debe retornar el primero en aparecer en el arreglo.
+Ejemplo: longest(["o","Hola","Pepe","Eh"], 4) → "Hola"
+*/
+
 char* longest(char* v[], int size) {
 
     char* str_maslargo = v[0];
@@ -22,16 +31,39 @@ char* longest(char* v[], int size) {
 }
 
 
+/*
+ b. Implementar la función char* superConcatenate(char* v[], int size), 
+ que toma un arreglo de punteros a string y retorna la concatenación de 
+ todos los elementos del arreglo.
+Ejemplo: superConcatenate(["Eh_","oo oo","+Gol"], 3) → "Eh_oo oo+Gol"
+*/
 char* superConcatenate(char* v[], int size) {
-
-    // COMPLETAR
+    char concatenacion = ""; // Variable que va a contener la concatenación 
+    for (int i = 0; i < size; i++) { // Recorro el arreglo de strings
+        concatenacion = concatenate(concatenacion, v[i]); // Concateno el string actual con la concatenación
+    }
+    return concatenacion;
 
     return 0;
 }
 
+/*
+c. Modificar la función anterior para soportar un nuevo parámetro de tipo string. 
+Este parámetro se utilizará para concatenarse entre cada par de string del arrelgo. 
+La aridad de esta función será: char* superConcatenateWithSep(char* v[], int size, char* s).
+Ejemplo: superConcatenateWithSep(["Hola","Pepe","Tito"], 3, "..") → "Hola..Pepe..Tito"
+*/
+
 char* superConcatenateWithSep(char* v[], int size, char* s) {
 
-    // COMPLETAR
+    char concatenacion = ""; // Variable que va a contener la concatenación
+    for (int i = 0; i < size; i++) { // Recorro el arreglo de strings
+        concatenacion = concatenate(concatenacion, v[i]); // Concateno el string actual con la concatenación
+        if (i != size - 1) { // Si no es el último string, concateno el separador
+            concatenacion = concatenate(concatenacion, s);
+        }
+    }
+    
 
     return 0;
 }
@@ -41,9 +73,9 @@ int main() {
     // -- Descomentar para probar --
     // Lo siguiente es un ejemplo y DEBE ser modificado.
 
-    char* v[5] = {"hola", "como", "va", "el", "dia"};
+    char* v[5] = {"supercalifragilisticoespialidoso", "como", "va", "el", "dia","supercalifragilisticoespialidososupercalifragilisticoespialidoso"};
 
-    char* l = longest(v, 5);
+    char* l = longest(v, 6);
 
     printf("El string mas largo es: \"%s\"\n", l);
 
