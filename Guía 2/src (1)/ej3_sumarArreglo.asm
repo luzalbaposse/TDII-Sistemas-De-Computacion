49acced73152528405548fd19e0d984f3bcba045
Luz Alba Posse <luzalbaposse@gmail.com>
2023-06-01 08:27:32 -0300

    ej3 y ej5

--- Guía 2/src (1)/ej3_sumarArreglo.asm
+++ Guía 2/src (1)/ej3_sumarArreglo.asm
@@ -1,3 +1,8 @@
+; Ejercicio 3: Dado un arreglo de 10 numeros de 32 bits en complemento a dos. Realizar la suma de todos los valores e imprimir en pantalla si la suma total resultó en un número positivo o negativo
+; Ejemplo:
+; arreglo: [-10, 2, -8, 17, -1, 20, -9, 52, 4, 12], resultado: Positivo
+; arreglo: [-10, 2, -8, -7, -1, -1, -9, -2, 4, 12], resultado: Negativo
+
 section .data
   arreglo: dd -1,2,3,4,-5,6,7,8,9,-10
                                
@@ -16,6 +21,14 @@ section .text
     mov rbx, 0     ; codigo    
     int 0x80                   
 
-sumarArreglo:
-  ; COMPLETAR
+sumarArreglo: ; Realiza la suma de todos los valores del arreglo y deja el resultado en eax
+    mov eax, 0 ; resultado
+    mov ebx, 0 ; indice
+    mov ecx, 10 ; cantidad de elementos
+    mov esi, arreglo ; direccion del arreglo
+    sumar: ; Suma los elementos del arreglo
+        add eax, [esi + ebx * 4] ; eax = eax + arreglo[ebx]
+        inc ebx ; ebx++
+        loop sumar ; Repite el ciclo hasta que ecx = 0
+    ret
   ret
--- Guía 2/src (1)/ej5_3numeros.asm
+++ Guía 2/src (1)/ej5_3numeros.asm
@@ -1,3 +1,9 @@
+; Ejercicio 5: Dados 3 números enteros de 8 bits, 16 bits y 32 bits respectivamente en complemento a 2, determinar si los tres n´umeros son positivos, o son negativos, o si hay positivos y negativos entre
+; los tres números. Imprimir en pantalla el resultado.
+; Ejemplo:
+; números: [10, -200, 3233], resultado: Hay positivos y negativos
+; números: [-21, -21, -300], resultado: Son todos negativos
+; números: [111, 411, 8934], resultado: Son todos positivos
 section .data
   numero1: db -10
   numero2: dw -9
@@ -17,6 +23,34 @@ section .text
     mov rbx, 0     ; codigo    
     int 0x80                   
 
-tresNumeros:
-  ; COMPLETAR
+tresNumeros: ; Esta función compara los tres números y determina si son positivos, negativos o hay positivos y negativos entre los tres números
+
+    ; Comparación de los tres números
+    cmp byte [numero1], 0
+    jg positivo1
+    cmp byte [numero1], 0
+    jl negativo1
+    cmp word [numero2], 0
+    jg positivo2
+    cmp word [numero2], 0
+    jl negativo2
+    cmp dword [numero3], 0
+    jg positivo3
+    cmp dword [numero3], 0
+    jl negativo3
+
+    ; Si no se cumple ninguna de las condiciones anteriores, entonces los tres números son positivos
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, positivos
+    mov edx, positivos_len
+    int 0x80
+
+    ; Exit
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
   ret
+
+; b. Si el primer número fuera de 64 bits, ¿qué modificaciones habría que hacer en el código?
+; Para que el primer número sea de 64 bits, se debe cambiar la etiqueta numero1 a dq y cambiar el registro de comparación a rcx
