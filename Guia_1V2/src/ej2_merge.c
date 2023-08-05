f1c7ce099777d85eeb665be71d402cae7a9d0d96
Luz Alba Posse <luzalbaposse@gmail.com>
2023-08-05 17:34:14 -0300

    fix

--- Guia_1V2/src/ej2_merge.c
+++ Guia_1V2/src/ej2_merge.c
@@ -36,6 +36,42 @@ int* merge(int* A, int sizeA, int* B, int sizeB) {
     }
     return C;
 }
+// modifico la funcion para que ahora devuelva un array pero de floats, aunque reciba arrays de enteros
+float* merge2(int* A, int sizeA, int* B, int sizeB) {
+    /*
+    Recibe: dos arreglos de enteros ordenados de menor a mayor.
+    Devuelve: un nuevo arreglo que combine ambos arreglos de forma ordenada.
+    */
+    float* C = malloc(sizeof(float) * (sizeA + sizeB));
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    while (i < sizeA && j < sizeB){
+        if (A[i] < B[j]){ // si el de a es msas chico que el de b
+            C[k] = A[i]; // pongo el de a
+            i++;
+            k++;
+        } 
+        else{
+            C[k] = B[j]; // sino pongo el de b
+            j++;
+            k++;
+        }
+    }
+   // si no tengo más elementos de A, pongo los de B
+    while (j < sizeB){
+        C[k] = B[j];
+        j++;
+        k++;
+    }
+    // si no tengo más elementos de B, pongo los de A
+    while (i < sizeA){
+        C[k] = A[i];
+        i++;
+        k++;
+    }
+    return C;
+}
 
 int main() {
 
@@ -51,6 +87,16 @@ int main() {
     printf("\n");
 
     free(result);
+    // testo merge2
+    int c[5] = {4,5,6,7,8};
+    int d[4] = {1,2,3,9};
+
+    float* result2 = merge2(c, 5, d, 4);
+
+    printf("Result:");
+    for(int i=0; i<9; i++) {
+        printf(" %f", result2[i]);
+    }
 
 
     return 0;
