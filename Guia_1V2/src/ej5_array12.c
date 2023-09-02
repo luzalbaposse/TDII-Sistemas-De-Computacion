0a775842e1d67da2b6c72aeb8a435d12590c0095
Luz Alba Posse <luzalbaposse@gmail.com>
2023-09-02 16:14:00 -0300

    ej 5

--- Guia_1V2/src/ej5_array12.c
+++ Guia_1V2/src/ej5_array12.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+Para ambos items se debe comprender la operaci´on de Type Casting o de conversi´on de tipos. Se
+debe definir una variable tipo puntero del tipo al que queremos ir, y luego se debe interpretar el
+puntero como el tipo de la variable indique. De esta forma, tendremos dos punteros exactamente
+iguales, apuntando a la misma memoria, pero que C los interpreta de forma diferente, porque son
+de tipos diferentes.
+*/
 int sumAsBytes(char* v) {
-
-    // COMPLETAR
-    
-    return 0;
+    /*
+    Interpretar al arreglo como un puntero a char* (enteros de un byte) y calcular la sumatoria de
+    los datos almacenados. 
+    */
+    int res = 0;
+    int contador = 0;
+    while (contador < 12){
+        res += v[contador];
+        contador ++;
+    }
+    return res;
 }
 
 int sumAsInts(int* v) {
-
-    // COMPLETAR
-    
-    return 0;
+    /*
+    Puntero a int* (enteros de 4 bytes), calcular la sumatoria de los datos
+    */
+   int res = 0;
+   int contador = 0;
+   for(contador = 0; contador < 3; contador ++){
+       res += v[contador];
+   }
+    return res;
 }
 
 int main() {
-    /*
-    // -- Descomentar para probar --
-    // Lo siguiente es un ejemplo y DEBE ser modificado.
 
     char v[12] = {0x05,0x01,0x00,0x00,0x05,0x01,0x00,0x00,0x05,0x01,0x00,0x00};
 
